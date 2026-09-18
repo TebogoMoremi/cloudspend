70145398af7e765b5f8d5ba4695d32ea9c5e6794
Tebogo <herroldmoremi@hotmail.com>
2026-09-19 00:14:21 +0200

    initial commit

---
+++ backend/src/main/java/com/tebogo/cloudspend_api/CloudspendApiApplication.java
@@ -0,0 +1,13 @@
+package com.tebogo.cloudspend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudspendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudspendApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tebogo/cloudspend_api/CloudspendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tebogo.cloudspend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudspendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
